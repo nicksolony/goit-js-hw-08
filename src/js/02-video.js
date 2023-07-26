2f782c8268943a761f00f1157adb3f68ea14cd69
nicksolony <nicksolony@gmail.com>
2023-07-26 17:46:29 -0400

    trying to get js to work on task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+console.log('agkjasjdka');
 
+let a=5;
+
+console.log(a);
+// import Player from '@vimeo/player';
+
+// // const player = new Player('handstick', {
+// //     id: 236203659,
+// //     width: 640
+// // });
+
+// // player.on('play', function() {
+// //     console.log('played the video!');
+// // });
+
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+// console.log(player)
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function(title) {
+//     console.log('title:', title);
+// });
