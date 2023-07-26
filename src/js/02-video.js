6e5eded56eb1d914b2c04030e52408f7ed6631a8
nicksolony <nicksolony@gmail.com>
2023-07-26 18:01:53 -0400

    imported vimeo player in video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,14 @@
-console.log('agkjasjdka');
-
-let a=5;
-
-console.log(a);
-// import Player from '@vimeo/player';
-
-// // const player = new Player('handstick', {
-// //     id: 236203659,
-// //     width: 640
-// // });
-
-// // player.on('play', function() {
-// //     console.log('played the video!');
-// // });
+import Player from '@vimeo/player';
 
 
-// const iframe = document.querySelector('iframe');
-// const player = new Vimeo.Player(iframe);
-// console.log(player)
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+console.log(player)
 
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
+player.on('play', function() {
+    console.log('played the video!');
+});
 
-// player.getVideoTitle().then(function(title) {
-//     console.log('title:', title);
-// });
+player.getVideoTitle().then(function(title) {
+    console.log('title:', title);
+});
