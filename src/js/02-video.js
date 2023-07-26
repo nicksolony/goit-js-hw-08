88e6e8dba23b52805437b6107e2bed04eb888487
nicksolony <nicksolony@gmail.com>
2023-07-26 18:23:12 -0400

    setting up script to get current time

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,12 +3,36 @@ import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-console.log(player)
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+console.log(player);
+
+// const onPlay = {
+//     player.getCurrentTime().then(function(seconds) {
+//         // seconds = the current playback position
+//         console.log(seconds)
+//     }).catch(function(error) {
+//         // an error occurred
+//     });
+// }; 
+
+const onPlay = function(data) {
+    console.log(player.getCurrentTime().then(function(seconds) {
+        // seconds = the current playback position
+    }).catch(function(error) {
+        // an error occurred
+    })
+    )
+};
+
+
+
+
+player.on('timeupdate', onPlay);
 
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
 });
+
+
+
+
