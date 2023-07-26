df3971a10d77e51ea4463e7858cf0366149ba554
nicksolony <54452819+nicksolony@users.noreply.github.com>
2023-07-26 21:46:59 +0000

    Deploying to gh-pages from @ nicksolony/goit-js-hw-08@2f782c8268943a761f00f1157adb3f68ea14cd69 🚀

---
+++ 02-video.8525446e.js
@@ -0,0 +1,2 @@
+!function(){console.log("agkjasjdka");console.log(5)}();
+//# sourceMappingURL=02-video.8525446e.js.map
---
+++ 02-video.96197c71.js
@@ -0,0 +1,2 @@
+console.log("agkjasjdka");console.log(5);
+//# sourceMappingURL=02-video.96197c71.js.map
