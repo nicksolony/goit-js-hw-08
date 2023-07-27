b75f9d4830e02765839b5b266d975dd70531ef1b
nicksolony <nicksolony@gmail.com>
2023-07-26 21:35:35 -0400

    enabled saving form data to local storage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,23 @@
+const FORM_STATE = "feedback-form-state";
 
-console.log("asdasd");
+const form = document.querySelector(".feedback-form");
+
+let formData = {
+    email:"",
+    message:""
+};
+
+const recordFormData = function (e) {
+    
+    if (localStorage.getItem(FORM_STATE)) {
+        formData = JSON.parse(localStorage.getItem(FORM_STATE))
+    };
+    
+    formData[`${e.target.name}`] = e.target.value;
+
+    localStorage.setItem(FORM_STATE, JSON.stringify(formData))
+    
+}
+
+form.addEventListener('input', recordFormData);
+
