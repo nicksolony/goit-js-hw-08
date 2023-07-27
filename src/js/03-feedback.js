f025c1222b38b3c8b402e822f43e2731dc0a986f
nicksolony <nicksolony@gmail.com>
2023-07-26 22:42:37 -0400

    added formdata from localstorage

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,14 +2,15 @@ const FORM_STATE = "feedback-form-state";
 
 const form = document.querySelector(".feedback-form");
 
-let formData = {
-    email:"",
-    message:""
-};
+
+checkLocalStorage();
+
+if (formData) {
+    form.email.value = formData.email;
+    form.message.value = formData.message;   
+}
 
 const recordFormData = function (e) {
-    
-    checkLocalStorage();
     
     formData[`${e.target.name}`] = e.target.value;
 
@@ -24,7 +25,12 @@ form.addEventListener('input', recordFormData);
 function checkLocalStorage() {
 
     if (localStorage.getItem(FORM_STATE)) {
-        formData = JSON.parse(localStorage.getItem(FORM_STATE))
+       return formData = JSON.parse(localStorage.getItem(FORM_STATE))
+    } else {
+        return formData = {
+            email:"",
+            message:""
+        };
     };
     
 };
